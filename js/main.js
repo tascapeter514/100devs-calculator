6b957f82cc549cc681d445279aff28fdda9b1876
petertasca <tascapeter@gmail.com>
2024-12-11 16:40:09 -0500

    calc working -- fix reset bug

--- js/main.js
+++ js/main.js
@@ -15,38 +15,65 @@
 // let divide = document.querySelector('#divide')
 
 let buttons = document.querySelectorAll('button')
-console.log(buttons)
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => displayText(button.textContent))
-})
-
 let num1 = ''
+let operator = ''
 let num2 = ''
 
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => storeVals(button))
+})
+
+
+
+
 //when we add a val, we want it to concatenate with the rest of the vals
-function displayText(val) {
-    console.log(val, typeof val)
-    nums += val
-    // let num = new MakeNum(val)
-    // console.log(num)
-    document.querySelector('h1').innerText = nums
-}
-
-class MakeNum {
-    constructor(operator, ...num1, ...num2) {
-        this.num = [...num]
+function storeVals(btn) {
+    let val = btn.textContent;
+    let calc;
+    if (btn.className === 'operator' && num1.length < 1) {
+        document.querySelector('h1').innerText = 'You must enter a number to begin'
+    } else if (btn.className === 'operator' && num1.length >= 1) {
+        operator += val
+        document.querySelector('h1').innerText += operator
+    } else if (btn.className === 'number' && operator.length < 1) {
+        num1 += val
+        document.querySelector('h1').innerText = num1
+    } else if (btn.className === 'number' && operator.length >= 1) {
+        num2 += val
+        document.querySelector('h1').innerText += val
+    } else if (btn.id === 'equal' && num1.length >= 1 && operator.length >= 1 && num2.length >= 1) {
+        calc = new MakeCalculation(num1, operator, num2)
+        console.log(calc)
+        document.querySelector('h1').innerText = calc.getCalc()
     }
-    getNumber() {
-        this.num.reduce((acc, curr) => acc + curr)
+}
+
+class MakeCalculation {
+    constructor(num1, operator, num2) {
+        this.num1 = Number(num1)
+        this.operator = operator
+        this.num2 = Number(num2) 
+    }
+    getCalc() {
+        let calc;
+        if (this.operator === '+') {
+            calc = this.num1 + this.num2
+        } else if (this.operator === '-') {
+            calc = this.num1 - this.num2
+        } else if (this.operator === '/') {
+            calc = this.num1 / this.num2
+        } else if (this.operator === 'x') {
+            calc = this.num1 * this.num2
+        }
+        return calc
     }
 }
 
-function storeVals(val) {
-    localStorage.setItem('num', val)
-}
+// function storeVals(val) {
+//     localStorage.setItem('num', val)
+// }
 
-console.log(document.querySelectorAll('.number'))
-console.log(document.querySelectorAll('.operator'))
+// console.log(document.querySelectorAll('.number'))
+// console.log(document.querySelectorAll('.operator'))
 
